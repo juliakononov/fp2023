c2bb97155776af63272f1be8a69015b5b24966ec
juliakononv <julickk04@mail.ru>
2023-10-30 02:10:34 +0300

    CSharpOOP: feat: add parser

--- CSharpOOP/lib/parser.mli
+++ CSharpOOP/lib/parser.mli
@@ -1,18 +1,8 @@
-(** Copyright 2021-2023, Kakadu and contributors *)
+(** Copyright 2021-2023, Julia Kononova *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
+open Ast
 
-type error = [ `ParsingError of string ]
-
-val pp_error : Format.formatter -> [< `ParsingError of string ] -> unit
-
-(** Main entry of parser *)
-val parse : string -> (Ast.name Ast.t, [> error ]) result
-
-type dispatch =
-  { apps : dispatch -> Ast.name Ast.t Angstrom.t
-  ; single : dispatch -> Ast.name Ast.t Angstrom.t
-  }
-
-(* A collection of miniparsers *)
-val parse_lam : dispatch
+val parse : 'a Angstrom.t -> string -> 'a option
+val parse1 : 'a Angstrom.t -> string -> ('a, string) result
+val e_method : methods Angstrom.t
