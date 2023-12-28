38b5af1b8a9cc33abd04a78053f5236701705902
juliakononv <julickk04@mail.ru>
2023-12-28 16:40:59 +0300

    CSharpOOP: feat: add interface parser

--- CSharpOOP/lib/ast.ml
+++ CSharpOOP/lib/ast.ml
@@ -27,10 +27,16 @@ type not_nullable_type =
   | TRef of ref_type
 [@@deriving show { with_path = false }, eq]
 
-type types =
+type type_ =
   | TNot_nullable_type of not_nullable_type
   | TNullable_type of base_type
+[@@deriving show { with_path = false }, eq]
+
+type var_type = TVar of type_ [@@deriving show { with_path = false }, eq]
+
+type meth_type =
   | TVoid (** type [void] for methods *)
+  | TRetrun of type_
 [@@deriving show { with_path = false }, eq]
 
 type name = Name of string [@@deriving show { with_path = false }, eq]
@@ -78,27 +84,55 @@ type un_op =
 [@@deriving show { with_path = false }, eq]
 
 type expr =
+  | Exp_Const of value
   | Bin_op of bin_op * expr * expr
   | Un_op of un_op * expr
-  | Var_Declaration of types * name (** Variable declaration [int a] *)
   | Access_By_Point of expr * expr (**  Access by point to class member [a.b.c]*)
-  | Exp_Assign of expr * expr (** Declare and assign a value to a variable [int a = 1]*)
-  | Exp_Val of value
   | Exp_Name of name
   | Exp_Args of expr list
   | Method_invoke of expr * expr (** Calling a method [Foo.a(i, 9)] *)
-  | Exp_Body of expr list (** Method body*)
-  | Exp_Class of class_modifier option * name * member list option
-  | If_Else of expr * expr * expr option
-  | Return of expr option
 [@@deriving show { with_path = false }, eq]
 
-and field = Field of (*field_modifier list option **) types * name * expr option
+type var_declaration =
+  | Var_Declaration of var_type * name (** Variable declaration [int a] *)
+[@@deriving show { with_path = false }, eq]
 
-and methods =
-  | Main
-  | Method of method_modifier list option * types * name * expr * expr
+type params = Params of var_declaration list [@@deriving show { with_path = false }, eq]
 
-and member =
-  | CField of field
-  | CMethod of methods
+type statement =
+  | Expr of expr
+  | Decl of var_declaration * expr option
+  (** Declare and assign a value to a variable [int a = 1]*)
+  | If of expr * statement * statement option
+  | Return of expr option
+  | Body of statement list
+[@@deriving show { with_path = false }, eq]
+
+type field = Field of field_modifier list option * var_type * name
+[@@deriving show { with_path = false }, eq]
+
+type methods = Method of method_modifier list option * meth_type * name * params
+[@@deriving show { with_path = false }, eq]
+
+type field_sign = Field_Sign of field * expr option
+[@@deriving show { with_path = false }, eq]
+
+type method_sign = Method_Sign of methods * statement
+[@@deriving show { with_path = false }, eq]
+
+type member =
+  | CField of field_sign
+  | CMethod of method_sign
+[@@deriving show { with_path = false }, eq]
+
+type i_member =
+  | IField of field
+  | IMethod of methods
+[@@deriving show { with_path = false }, eq]
+
+type objects =
+  | Class of class_modifier list option * name * member list
+  | Interface of class_modifier option * name * i_member list
+[@@deriving show { with_path = false }, eq]
+
+type program = Ast of objects list option [@@deriving show { with_path = false }, eq]
--- CSharpOOP/lib/parser.mli
+++ CSharpOOP/lib/parser.mli
@@ -2,7 +2,16 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-open Ast
-
+val method_invoke : Ast.expr Angstrom.t -> Ast.expr Angstrom.t
+val s_declaration : Ast.statement Angstrom.t
+val s_if_else : Ast.statement Angstrom.t -> Ast.statement Angstrom.t
+val p_body : Ast.statement Angstrom.t
+val p_method : Ast.methods Angstrom.t
+val c_method : Ast.method_sign Angstrom.t
+val p_field : Ast.field Angstrom.t
+val p_class : Ast.objects Angstrom.t
+val p_interface : Ast.objects Angstrom.t
+val p_ast : Ast.program Angstrom.t
 val parse : 'a Angstrom.t -> string -> ('a, string) result
-val e_method : methods Angstrom.t
+val parse_to_some : 'a Angstrom.t -> string -> 'a option
+val assign_option : Ast.expr option Angstrom.t
