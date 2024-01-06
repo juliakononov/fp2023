e0d28082e3a4cb2520d47a4f644c19b8920d652c
Kuarni <kuarni22@gmail.com>
2024-01-06 21:16:39 +0300

    refactor(print): use autoformat

--- Javascript/lib/print.ml
+++ Javascript/lib/print.ml
@@ -1,22 +1,18 @@
 (** Copyright 2023, Kuarni, AlexShmak *)
+
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 open Parser
 open Ast
 
-let pp_ok result =
-  Format.printf "%a" pp_statement @@ Result.get_ok(result)
-;;
+let pp_ok result = Format.printf "%a" pp_statement @@ Result.get_ok result
 
-let pp_error result = match Result.get_error result with
+let pp_error result =
+  match Result.get_error result with
   | `ParsingError s -> Format.eprintf "Error: %s" s
 ;;
 
-let pp ?(parse=parse) str =
+let pp ?(parse = parse) str =
   let result = parse str in
-  if Result.is_ok(result) then
-    pp_ok result
-  else (
-    pp_error result
- )
+  if Result.is_ok result then pp_ok result else pp_error result
 ;;
--- Javascript/lib/print.mli
+++ Javascript/lib/print.mli
@@ -1,8 +1,7 @@
 (** Copyright 2023, Kuarni, AlexShmak *)
+
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-val pp_ok: (Ast.statement, 'a) result -> unit
-
-val pp_error: ('a, [< `ParsingError of string ]) result -> unit
-
-val pp: ?parse:(string -> (Ast.statement, Parser.error) result) -> string -> unit
+val pp_ok : (Ast.statement, 'a) result -> unit
+val pp_error : ('a, [< `ParsingError of string ]) result -> unit
+val pp : ?parse:(string -> (Ast.statement, Parser.error) result) -> string -> unit
