261a60057daab062419b224549bc42121eb0bbd1
Kuarni <kuarni22@gmail.com>
2024-01-09 02:43:45 +0300

    fix(utils): fix fold_left_map type

--- Javascript/lib/utils.mli
+++ Javascript/lib/utils.mli
@@ -11,5 +11,5 @@ val return : 'a -> 'a t
 val uerror : string -> 'a t
 val map : ('a -> 'b t) -> 'a list -> 'b list t
 val fold_left : ('a -> 'b -> 'a t) -> 'a -> 'b list -> 'a t
-val fold_left_map : ('a -> 'b -> ('a * 'b) t) -> 'a -> 'b list -> ('a * 'b list) t
+val fold_left_map : ('a -> 'b -> ('a * 'c) t) -> 'a -> 'b list -> ('a * 'c list) t
 val fold_left_s : ('a -> 'b -> 'a t) -> ('a -> bool) -> 'a -> 'b list -> 'a t
