3c4d8301586a506c4bfe9d62b4cc7932a3af3fa2
Kuarni <kuarni22@gmail.com>
2024-01-09 00:02:36 +0300

    feat(utils): add fold_left_map
    also beutify code of other utils

--- Javascript/lib/utils.ml
+++ Javascript/lib/utils.ml
@@ -17,15 +17,22 @@ let ( <?> ) x str =
 
 let rec map f = function
   | [] -> return []
-  | h :: tl -> f h >>= fun c -> map f tl >>= fun lst -> return (c :: lst)
+  | h :: tl -> f h >>= fun c -> map f tl >>| fun lst -> c :: lst
 ;;
 
 let rec fold_left f acc = function
   | [] -> return acc
-  | a :: l -> f acc a >>= fun x -> fold_left f x l
+  | h :: tl -> f acc h >>= fun acc -> fold_left f acc tl
+;;
+
+let rec fold_left_map f acc = function
+  | [] -> return (acc, [])
+  | h :: tl ->
+    f acc h >>= fun (acc, c) -> fold_left_map f acc tl >>| fun (acc, lst) -> acc, c :: lst
 ;;
 
 let rec fold_left_s f stop acc = function
   | [] -> return acc
-  | a :: l -> f acc a >>= fun x -> if stop x then return x else fold_left_s f stop x l
+  | h :: tl ->
+    f acc h >>= fun acc -> if stop acc then return acc else fold_left_s f stop acc tl
 ;;
