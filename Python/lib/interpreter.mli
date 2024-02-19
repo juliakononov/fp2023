a83690c19ad9b2894d539ee780333ec487119240
Pavel Averin <pasha2281337alfa@gmail.com>
2024-02-19 16:13:38 +0300

    remove extra functions from interpreter.mli

--- Python/lib/interpreter.mli
+++ Python/lib/interpreter.mli
@@ -29,9 +29,6 @@ module Result : sig
 end
 
 module Eval : functor (M : MONADERROR) -> sig
-  val fold_left : ('a -> 'b -> 'a M.t) -> 'a -> 'b list -> 'a M.t
-  val map1 : ('a -> 'b M.t) -> 'a list -> 'b list M.t
-
   type var_symb =
     { identifier : Ast.identifier
     ; value : Ast.value
@@ -54,32 +51,7 @@ module Eval : functor (M : MONADERROR) -> sig
     ; vals_to_print : Ast.value list
     }
 
-  val local_env : environment
-  val temp_class_env : environment
-  val global_env : environment
-  val var_in_env : Ast.identifier -> environment -> bool
-  val change_var : Ast.identifier -> Ast.value -> environment -> environment
-  val change_or_add_var : environment -> var_symb -> environment
-  val change_or_add_var_list : environment -> var_symb list -> environment
-  val get_var : Ast.identifier -> environment -> var_symb
-  val func_in_env : Ast.identifier -> environment -> bool
-  val class_in_env : Ast.identifier -> environment -> bool
-  val change_func : function_symb -> environment -> environment
-  val change_or_add_func : function_symb -> environment -> environment
-  val change_class : environment -> environment -> environment
-  val change_or_add_class : environment -> environment -> environment
-  val get_func : Ast.identifier -> environment -> function_symb
-  val get_class : Ast.identifier -> environment -> environment
-  val combine_args_and_params : Ast.identifier list -> Ast.value list -> var_symb list
   val get_str_from_identifier : Ast.identifier -> string
   val pack_to_string : Ast.value -> string M.t
-
-  type dispatch =
-    { i_expr : dispatch -> environment -> Ast.expression -> Ast.value M.t
-    ; i_stmt : dispatch -> environment -> Ast.statement -> environment M.t
-    }
-
-  val i_exp_or_stmt : dispatch
-  val get_env : environment -> Ast.statement list -> environment M.t
   val interpret : Ast.statement list -> environment M.t
 end
